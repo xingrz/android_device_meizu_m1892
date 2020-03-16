a987e861aa881e7535251955be4ef97a82bea4c4
XiNGRZ <chenxingyu92@gmail.com>
2020-03-16 18:37:24 +0800

    m1892: Build sensors HAL
    Change-Id: Iad6fbfad6038a9eebcb25ed1eb87a477a23a01f7

--- device.mk
+++ device.mk
@@ -11,5 +11,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-mokee
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+
 # Inherit from sdm845-common
 $(call inherit-product, device/meizu/sdm845-common/common.mk)
